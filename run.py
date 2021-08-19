c9e97429bf62f45f618c30fcb65f05346ab56f72
daisygunn <daisy.l.g@hotmail.co.uk>
2021-08-19 17:04:02 +0000

    Add inital code for user_choice function. & add outlines for additional functions

--- run.py
+++ run.py
@@ -14,14 +14,57 @@ SHEET = GSPREAD_CLIENT.open('Contacts sheet')
 
 
 # User choice
+def user_choice():
+    """
+    User selects which task they would like to do
+    Take their input and runs elif loop to trigger next process.
+    If invalid choice is input then will continue to ask for a
+    valid input.
+    """
+    print(
+        "Please select from the following options:")
+    print(
+        "1. Retrieve all contacts 2. Retreive specific contact \
+             3.Add new contact 4.Edit existing contact")
+
+    user_input = input('Type your input here...')
+    try:
+        if user_input == 1:
+            retrieve_all_contacts()
+        elif user_input == 2:
+            retrieve_one_contact
+        elif user_input == 3:
+            add_new_contact()
+        elif user_input == 4:
+            edit_existing_contact
+    except ValueError as e:
+        print(f"You have input an invalid option {e}, \
+            please select a number between 1 & 4.")
+
 
 # Retrieve all contacts
+def retrieve_all_contacts():
+    """
+    """
+
 
 # Retrieve one contact
+def retrieve_one_contact():
+    """
+    """
+
 
 # Add new contact
+def add_new_contact():
+    """
+    """
+
 
 # Edit existing contact
+def edit_existing_contact():
+    """
+    """
+
 
 # Update worksheet
 
@@ -33,4 +76,7 @@ def run_programme():
     to run the programme
     """
     print('Welcome to your contacts book application!\n')
-    
+    user_choice()
+
+
+run_programme()
