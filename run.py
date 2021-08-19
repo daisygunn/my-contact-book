f7bde797bd8450ac62b08e24c24eafb1c5aca59d
daisygunn <daisy.l.g@hotmail.co.uk>
2021-08-19 16:13:21 +0000

    Add gspread & google auth, & connect contacts sheet

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Contacts sheet')
+
+
 # User choice
 
 # Retrieve all contacts
@@ -11,3 +26,11 @@
 # Update worksheet
 
 # Save
+
+def run_programme():
+    """
+    This function will call on all of the other functions
+    to run the programme
+    """
+    print('Welcome to your contacts book application!\n')
+    
