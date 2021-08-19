fb1915bfbd35e1258c7b41c8703959842c7153e7
daisygunn <daisy.l.g@hotmail.co.uk>
2021-08-19 17:25:52 +0000

    Add to user_choice function

--- run.py
+++ run.py
@@ -24,51 +24,64 @@ def user_choice():
     print(
         "Please select from the following options:")
     print(
-        "1. Retrieve all contacts 2. Retreive specific contact \
-             3.Add new contact 4.Edit existing contact")
+        "1. Retrieve all contacts 2. Retreive specific contact\
+        3.Add new contact 4.Edit existing contact")
 
     user_input = input('Type your input here...')
-    try:
-        if user_input == 1:
-            retrieve_all_contacts()
-        elif user_input == 2:
-            retrieve_one_contact
-        elif user_input == 3:
-            add_new_contact()
-        elif user_input == 4:
-            edit_existing_contact
-    except ValueError as e:
-        print(f"You have input an invalid option {e}, \
-            please select a number between 1 & 4.")
+
+    # while True:
+    #     try:
+    if user_input == 1:
+        retrieve_all_contacts()
+    elif user_input == 2:
+        retrieve_one_contact
+    elif user_input == 3:
+        add_new_contact()
+    elif user_input == 4:
+        edit_existing_contact()
+        # except ValueError as e:
+        #     print(f"You have input an invalid option {e},\
+        #         please select a number between 1 & 4.")
+        #     return False
+
+        # return True
 
 
 # Retrieve all contacts
 def retrieve_all_contacts():
     """
+    Function to retrieve full list of contacts
     """
+    print('Retrieve all')
 
 
 # Retrieve one contact
 def retrieve_one_contact():
     """
+    Allows user to search for specific contact
     """
+    print('Retrieve one')
 
 
 # Add new contact
 def add_new_contact():
     """
+    Allows user to add new contact information
     """
+    print('Add')
 
 
 # Edit existing contact
 def edit_existing_contact():
     """
+    Allows user to edit exiting contact
     """
-
+    print('Edit')
 
 # Update worksheet
 
 # Save
+
 
 def run_programme():
     """
